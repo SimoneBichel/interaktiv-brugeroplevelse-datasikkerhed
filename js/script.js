5fee69130ccc1c7d71ec06a24afb19d342ec4b05
SimoneBichel <simbic01@iba.dk>
2026-06-21 12:24:31 +0200

    js-kommentar

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//a single-line comment
+/*a multi-line comment
+that can span multiple lines
+and is useful for longer explanations or commenting out blocks of code*/
+
+//variabler oprettes med var, let eller const//
+
+//var er den ældste måde at oprette variabler på, og har nogle quirks, så det anbefales at bruge let eller const i moderne JavaScript
+
+/*//let bruges til variabler, der kan ændres senere
+let name = "Simon";
+name = "Emil"; // dette er tilladt
+
+//const bruges til variabler, der ikke skal ændres
+const pi = 3.14;
+//pi = 3.14159; // dette vil give en fejl, fordi pi er en konstant
+
+//datatyper i JavaScript
+let age = 30; // number
+let isStudent = true; // boolean
+let firstName = "Simon"; // string
+let hobbies = ["coding", "gaming", "traveling"]; // array
+let person = { name: "Simon", age: 30 }; // object
+*/
+
+
+
